a65137e87a32977a0df8949199254ce62b6dae76
xiaoran <tongxia1@msu.edu>
2015-12-04 03:20:38 -0500

    improve data preparation, directly make plink tped files, instead of making dosage data first.

--- src/prep.sh
+++ src/prep.sh
@@ -1,29 +1,31 @@
 ## prepare phase 2 twin genotype data
 cd dat/p2.0
 
-## extract genotype columns, drop R and Theta value from GenomeStudio
-cut g.txt -f1,2,3,$(seq -s, 4 3 $(head -n 1 g.txt | wc -w)) > g.nox
+## rearrange Illumina GenomicStudio output
+## 1) extract genotype variants, drop R and Theta values, and the the header
+## 2) transform X, Y, XY and MT chromosome to numeric coding
+## 3) sort the variants by chromosome position
+cut g.txt -f1,2,3,$(seq -s, 4 3 $(head -n 1 g.txt | wc -w)) | tail -n +2| g.txt.1
+sed g.txt.1 -e 's/\tX\t/\t23\t/' -e 's/\tY\t/\t24\t/' -e 's/\tXY\t/\t25\t/' -e 's/\tMT\t/\t26\t/' > g.txt.2
+sort -k 2g -k 3g g.txt.2 > g.txt.3
 
-## transform AA, BB, Ab coding to dosage values;
-## use '3' to represent NC (no calling)
-sed g.nox -e '1,1 s/\.GType//g' -e '2,$ s/AA/0/g' -e 's/BB/2/g' -e 's/AB/1/g' -e 's/NC/3/g' > g.dsg.0
+## get variant map out of sorted genotype data, rewrite in plink
+## map format
+awk '{print($2,"\t",$1,"\t","0","\t",$3)}' g.txt.3 > g.plink.map
 
-## transform X, Y, XY and MT chromosome to numeric coding
-sed g.dsg.0 -e 's/\tX\t/\t23\t/' -e 's/\tY\t/\t24\t/' -e 's/\tXY\t/\t25\t/' -e 's/\tMT\t/\t26\t/' > g.dsg.1
+## get value matrix from sorted genotype data, rewrite in a format
+## close to plink tped
+cut g.txt.3 -f4- | sed -e 's/\([AB]\)\([AB]\)/\1 \2/g' -e 's/NC/0 0/g' > g.plink.tped.mtx
 
-## sort the genome features by chromosome and basepair loacation
-sort -k 2g -k 3g g.dsg.1 > g.dsg.2
+## get subject ID, in the original order
+head -n 1 g.txt | cut --output-delimiter $'\n' -f$(seq -s, 4 3 $(head -n 1 g.txt | wc -w)) | tr -d '.GType' > g.txt.sbj
 
-## split genome feature map and dosage matrix
-cut -f1,2,3 g.dsg.2 > g.dsg.map && cut -f4- g.dsg.2 > g.dsg.3
-
-## remove subject IDs in the head of dosage matrix
-tail -n +2 g.dsg.3 > g.dsg.mtx
-
-## transform texture dosage format to plink tped format
-awk 'NR>1 {print($2,"\t",$1,"\t","0","\t",$3)}' g.dsg.map > g.plink.map
-sed g.dsg.mtx -e 's/0/A A/g' -e 's/1/A B/g' -e 's/2/B B/g' -e 's/3/0 0/g' > g.plink.tped.mtx
+## patch up the plink tped file, then creat plink binary files
 paste g.plink.map g.plink.tped.mtx >g.plink.tped
+plink --tped g.plink.tped --tfam g.plink.fam --out g.plink.0
+
+## haploid heterogeneity check
+plink --bfile g.plink.0 --set-hh-missing --make-bed --out g.plink.1
 
 ## prepare phase 1 twin genotype data
 cd dat/p1.0
